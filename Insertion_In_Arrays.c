5269defebe926d55fe96502e74732268877b057f
Sadiqa609 <64349633+Sadiqa609@users.noreply.github.com>
2024-04-30 10:06:01 +0500

    Further Code in insertion
    Coding the insertion in arrays

--- Insertion_In_Arrays.c
+++ Insertion_In_Arrays.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+void display(int array[],int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+}
 int main(){
-    int array[100];
-
-    
+    printf("The array traversal is shown below %d\n");
+    int array[100] = {1,2,5,79};
+    display(array, 4);
     return 0;
 }
